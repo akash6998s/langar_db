b65454fc2e468b3f66f7ed6932cbd8defd6baea2
Akash Singh <akash6998s@gmail.com>
2025-04-29 13:22:40 +0530

    update package.json

---
+++ keepAlive.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const url = 'https://langar-db-csvv.onrender.com/'; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
